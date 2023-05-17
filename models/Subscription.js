d064ecf5e9817add0911bd21b1aeb5f127322a53
Ivan Zavaleta <ivzavaleta@gmail.com>
2023-05-16 21:04:47 -0500

    fixed model relationships

--- models/Subscription.js
+++ models/Subscription.js
@@ -1,36 +1,36 @@
-const {  Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+// const {  Model, DataTypes } = require('sequelize');
+// const sequelize = require('../config/connection');
 
-class Subscription extends Model {}
+// class Subscription extends Model {}
 
-Subscription.init(
-  {
-    sub_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    event_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'event',
-        key: 'event_id'
-      },
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'user',
-        key: 'user_id',
-      },
-    },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'subscription',
-  }
-)
+// Subscription.init(
+//   {
+//     // sub_id: {
+//     //   type: DataTypes.INTEGER,
+//     //   allowNull: false,
+//     //   primaryKey: true,
+//     //   autoIncrement: true,
+//     // },
+//     // event_id: {
+//     //   type: DataTypes.INTEGER,
+//     //   references: {
+//     //     model: 'event',
+//     //     key: 'event_id'
+//     //   },
+//     // },
+//     // user_id: {
+//     //   type: DataTypes.INTEGER,
+//     //   references: {
+//     //     model: 'user',
+//     //     key: 'user_id',
+//     //   },
+//     // },
+//   },
+//   {
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     // modelName: 'subscription',
+//   }
+// )
--- models/index.js
+++ models/index.js
@@ -1,30 +1,30 @@
 const User = require('./User');
-const Event = require('./Event')
-const Comment = require('./Comment')
-const Subscription = require('./Subscription')
+const Event = require('./Event');
+const Comment = require('./Comment');
+// const Subscription = require('./Subscription')
 
 // user/event associations
 User.hasMany(Event, { // one organizer can create many events
   foreignKey: 'creator_id',
   onDelete: 'CASCADE',
-}); 
+});
 Event.belongsTo(User, { // each event belongs to a single creator/organizer/coordinator
   foreignKey: 'creator_id',
 });
 
 // user/event associations when subscribing
 User.belongsToMany(Event, { // since we want record of users subscribed to events we need a subscription table, which will create a new row
-  through: Subscription, // everytime the user clicks subscribe, taking in the specific event_id and specific user_id for the sub row. 
-  foreignKey: 'user_id', // this way we can query all events for a user by searching where all rows where their user_id exists, all the users for a
-  onDelete: 'CASCADE', // event when we query all by event_id, and delete event subscriptions for a user where both event_id and user_id match.
+  through: 'Subscription', // everytime the user clicks subscribe, taking in the specific event_id and specific user_id for the sub row. 
+  // foreignKey: 'user_id', // this way we can query all events for a user by searching where all rows where their user_id exists, all the users for a
+  // onDelete: 'CASCADE', // event when we query all by event_id, and delete event subscriptions for a user where both event_id and user_id match.
 });
 Event.belongsToMany(User, {
-  through: Subscription,
-  foreignKey: 'event_id',
-  onDelete: 'CASCADE'
+  through: 'Subscription',
+  // foreignKey: 'event_id',
+  // onDelete: 'CASCADE'
 });
 
-// user/comment associations 
+// user/comment associations
 User.hasMany(Comment, { // user creates many comments
   foreignKey: 'commenter_id',
   onDelete: 'CASCADE',
@@ -32,7 +32,14 @@ User.hasMany(Comment, { // user creates many comments
 Comment.belongsTo(User, { // each comment belongs to a single user
   foreignKey: 'commenter_id',
 });
+Event.hasMany(Comment, { // user creates many comments
+  foreignKey: 'event_id',
+  onDelete: 'CASCADE',
+});
+Comment.belongsTo(Event, { // each comment belongs to a single user
+  foreignKey: 'event_id',
+});
 
-module.exports = { User, Event , Comment, Subscription };
+module.exports = { User, Event , Comment, };
 
 // in this file we import all the models and define the associations between them, then export it back to the app
