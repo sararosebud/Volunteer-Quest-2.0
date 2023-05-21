322b0d27600550f103ac157d9aafeb2902014adf
Ivan Zavaleta <ivzavaleta@gmail.com>
2023-05-21 00:56:30 -0500

    fixed redundent routing files in controller that cause a error when going to login page

--- controllers/profileRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const { Event, User } = require('../models');
-const withAuth = require('../utils/auth');
-// all req that get sent from frontend to backend for profile page 
-
-
-module.exports = router;
