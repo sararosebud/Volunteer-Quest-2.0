a4d46088907b75a133d4fd16715efb46205449b4
Ivan Zavaleta <ivzavaleta@gmail.com>
2023-05-22 13:30:24 -0500

    readded allEvents route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -28,6 +28,32 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/events', async (req, res) => {
+  try {
+    // Get all events and JOIN with user data
+    const eventData = await Event.findAll({
+      include: [
+        {
+          model: User,
+          as: 'creator',
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const events = eventData.map((event) => event.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('allEvents', { 
+      events, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/event/:id', async (req, res) => {
   try {
     const eventData = await Event.findByPk(req.params.id, {
